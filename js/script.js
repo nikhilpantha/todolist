8fdc06572ca00950cc5db313a6825e84fffc202e
nikhilpantha <nikhilpantha207@gmail.com>
2021-06-09 22:46:30 +0545

    First commit

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ let email = document.getElementById("inputemail");
 let submit = document.getElementById("submitbutton");
 let todolist = document.getElementById("todolist");
 let errormessage = document.getElementById("message");
+console.log(localStorage);
+console.log(localStorage.length);
 
 //event validation
 
@@ -28,32 +30,9 @@ function addlist(e) {
   } else if (!email.value.match(emailrepex)) {
     errormessage.innerText = "Please enter Valid Email";
   } else {
-    // div
-    const tododiv = document.createElement("div");
-    tododiv.classList.add("tododiv");
-    // li tage
-    const todoli = document.createElement("div");
-    todoli.classList.add("todolist");
-    tododiv.appendChild(todoli);
-    // div of name inside of li tag
-    const namelist = document.createElement("div");
-    namelist.classList.add("namelist");
-    namelist.innerText = inname.value;
-    todoli.appendChild(namelist);
-    //div of email
-    const emaillist = document.createElement("div");
-    emaillist.classList.add("emaillist");
-    emaillist.innerText = email.value;
-    todoli.appendChild(emaillist);
-    //button in tododiv tag
-    const trashbutton = document.createElement("div");
-    trashbutton.classList.add("trashbutton");
-
-    trashbutton.innerHTML = '<i class="fas fa-trash" ></i>';
-    tododiv.appendChild(trashbutton);
-
-    //append to list
-    todolist.appendChild(tododiv);
+    //local storage
+    localStorage.setItem(inname.value, email.value);
+    location.reload();
 
     inname.value = "";
     email.value = "";
@@ -64,8 +43,47 @@ function addlist(e) {
 
 function deletebutton(e) {
   const clickitem = e.target;
-  if (clickitem.classList[0] === "trashbutton") {
-    const parentitem = clickitem.parentElement;
-    parentitem.remove();
-  }
+  const selectedelete = clickitem.parentElement.firstChild.firstChild.innerHTML;
+  localStorage.removeItem(selectedelete);
+  location.reload();
+}
+
+///local storage
+
+for (i = 0; i < localStorage.length; i++) {
+  //  div
+  const tododiv = document.createElement("div");
+  tododiv.classList.add("tododiv");
+  // li tage
+  const todoli = document.createElement("div");
+  todoli.classList.add("todolist");
+  tododiv.appendChild(todoli);
+  // div of name inside of li tag
+  const namelist = document.createElement("div");
+  namelist.classList.add("namelist");
+  // namelist.innerText = inname.value;
+  todoli.appendChild(namelist);
+  //div of email
+  const emaillist = document.createElement("div");
+  emaillist.classList.add("emaillist");
+  // emaillist.innerText = email.value;
+  todoli.appendChild(emaillist);
+  //button in tododiv tag
+  const trashbutton = document.createElement("div");
+  trashbutton.classList.add("trashbutton");
+  trashbutton.innerHTML = '<i class="fas fa-trash" ></i>';
+  tododiv.appendChild(trashbutton);
+
+  //append to list
+  todolist.appendChild(tododiv);
+
+  ///
+
+  const lsname = localStorage.key(i);
+  const lsemail = localStorage.getItem(lsname);
+
+  namelist.innerText = lsname;
+  emaillist.innerText = lsemail;
+  console.log(lsname);
+  console.log(lsemail);
 }
